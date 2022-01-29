85f09eb50d4a40e128447df851e066aecf7408f8
Kukarkin Nikolay <n.kukarkin1991@gmail.com>
2022-01-29 18:35:08 +0300

    коммит к домашнему заданию

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello!!!");
+let title = "";
+let screenPrice = "";
+let screens = "";
+let rollback = "";
+let fullPrice = "";
+let adaptive = "";
+
+//alert("сообщение с любым текстом");
+
+console.log("сообщение с любым текстом")
