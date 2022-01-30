6fcc7deabdc2ff46385e55068496a18deb4f35ca
Kukarkin Nikolay <n.kukarkin1991@gmail.com>
2022-01-30 14:34:28 +0300

    ДЗ урок 2

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let title, screenPrice, screens, rollback, fullPrice, adaptive;
+title = "Название проекта";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 1;
+rollback = 99;
+fullPrice = 1000000;
+adaptive = true;
 
-alert("сообщение с любым текстом");
 
-console.log("сообщение с любым текстом");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " доллар\(ов\)");
+console.log("Стоимость разработки сайта " + fullPrice + " доллар\(ов\)");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
